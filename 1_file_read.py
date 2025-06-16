165220e698b94440804c6f37588527865a873d11
“Arpita <arpitakumari10653@gmail.com>
2025-06-16 10:14:16 +0530

    read a file

---
+++ 1_file_read.py
@@ -0,0 +1,15 @@
+'''
+a = "a very long string with emails"
+
+emails = []
+3 second to gentrate     # persist = save 
+
+'''
+
+# Reading a file in python 
+f = open("file.txt")
+data = f.read()
+print(data)
+f.close()
+
+ 
