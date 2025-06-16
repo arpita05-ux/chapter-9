4756374d49d86f5c5d0e523bd9ef33be9b1f753a
“Arpita <arpitakumari10653@gmail.com>
2025-06-16 10:53:55 +0530

    read file line by line

---
+++ 3_more function of file.py
@@ -0,0 +1,17 @@
+f = open("file.txt")
+
+# lines = f.readlines()
+# print(lines, type(lines))
+
+# "or"
+
+line1 = f.readline()
+print(line1, type(line1))
+
+line2 = f.readline()
+print(line2, type(line2))
+f.close()
+
+
+ 
+
